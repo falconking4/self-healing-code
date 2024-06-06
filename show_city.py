9d8dccd0c4b0d281b28734cee9b90171d8bc31ca
falconking4 <50393561+falconking4@users.noreply.github.com>
2024-06-06 08:47:18 -0700

    Add files via upload

---
+++ show_city.py
@@ -0,0 +1,11 @@
+def show_city(info):
+    city = info["city"]
+    print("City:", city)
+
+info = {
+    "name": "Jessica Lee",
+    "country": "Canada"
+}
+
+if __name__ == "__main__":
+    show_city(info)
